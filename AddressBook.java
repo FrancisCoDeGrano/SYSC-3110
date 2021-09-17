35beeeac80460b2f483546ce6b70ada942e3d8cc
franciscodegrano <franciscodgrano@gmail.com>
2021-09-17 12:27:12 -0400

    Added println statement "AddressBook"

--- AddressBook.java
+++ AddressBook.java
@@ -34,15 +34,6 @@ public class AddressBook{
     }
 
     public static void main(String [] args){
-        BuddyInfo francisco = new BuddyInfo("Francisco");
-        BuddyInfo doro = new BuddyInfo("Doro");
-
-        AddressBook newf = new AddressBook(); //newfriend in address book
-
-        newf.addBuddy(francisco);
-        newf.addBuddy(doro);
-
-        System.out.println(newf);
         System.out.println("AddressBook");
 
     }
