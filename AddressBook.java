3fafb749079649712a13d735d0eb93bc02d91151
franciscodegrano <franciscodgrano@gmail.com>
2021-09-17 12:25:38 -0400

    Added println statement "AddressBook"

---
+++ AddressBook.java
@@ -0,0 +1,51 @@
+/*
+Francisco De Grano
+101147447
+
+I made changes to lab 1 used to only have get and set name
+added things relevant to an address book.
+ */
+
+import java.util.ArrayList;
+
+public class AddressBook{
+
+    private ArrayList<BuddyInfo> newfriend;
+
+    public AddressBook(){
+        this.newfriend = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        this.newfriend.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        this.newfriend.remove(buddy);
+    }
+
+    // toString method from SYSC 2004
+    public String toString(){
+        String result = "";
+        for(BuddyInfo buddy : this.newfriend){
+            result += buddy.toString() + '\n';
+        }
+        return result;
+    }
+
+    public static void main(String [] args){
+        BuddyInfo francisco = new BuddyInfo("Francisco");
+        BuddyInfo doro = new BuddyInfo("Doro");
+
+        AddressBook newf = new AddressBook(); //newfriend in address book
+
+        newf.addBuddy(francisco);
+        newf.addBuddy(doro);
+
+        System.out.println(newf);
+        System.out.println("AddressBook");
+
+    }
+
+
+}
